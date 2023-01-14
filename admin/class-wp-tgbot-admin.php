8d54506fdcd5a8752a6c329d61a1652df1a30f2b
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2023-01-14 22:28:49 +0700

    tambah kolom token bot

--- admin/class-wp-tgbot-admin.php
+++ admin/class-wp-tgbot-admin.php
@@ -110,15 +110,51 @@ class Wp_Tgbot_Admin {
 	        ->add_fields( array(
 				Field::make( 'html', 'crb_tgbot_halaman_terkait' )
 		        	->set_html( '
-					<h5>HALAMAN TERKAIT</h5>
+					<h5>Catatan</h5>
 	            	<ol>
 	            		<li>Video tutorial bisa dicek di ...</li>
+	            		<li>Buat Bot dengan mengirimkan teks <b>/newbot</b> ke akun <b>@BotFather</b>.</li>
+						<li>Setelah selesai, akun <b>@BotFather</b> akan memberikan anda Token Bot.</li>
+						<li>Copy token dan paste pada kolom Bot Token.</li>
 	            	</ol>
 		        	' ),
 	            Field::make( 'text', 'crb_apikey_tgbot', 'API KEY' )
 	            	->set_default_value($this->functions->generateRandomString())
 	            	->set_help_text('Wajib diisi. API KEY digunakan untuk integrasi data.')
+            		->set_required( true ),
+	            Field::make( 'text', 'crb_tgbot_token', 'Token Bot' )
+	            	->set_help_text('Wajib diisi. Token Bot Telegram yang sudah dibuat.')
+            		->set_required( true ),
+		        Field::make( 'text', 'crb_github_url', __( 'Endpoint API for GITHUB' ) )
+		        	->set_default_value(site_url().'/wp-admin/admin-ajax.php?action=send_tgbot&api_key='.get_option('_crb_apikey_tgbot').'&tg_id=[channel_id]')
+		        	->set_attribute('readOnly', 'true'),
+	            Field::make( 'complex', 'crb_tgbot_api', __( 'Telegram API' ) )
+				    ->add_fields( array(
+				        Field::make( 'radio', 'tg_mode', __( 'Parse mode' ) )
+			            	->add_options( array(
+						        '1' => __( 'HTML' ),
+						        '2' => __( 'Markdown' )
+						    ) )
+            				->set_default_value('1'),
+				        Field::make( 'text', 'tg_id', __( 'Chanel ID / Akun ID' ) ),
+				        Field::make( 'textarea', 'tg_message', __( 'Format Pesan' ) )
+	            			->set_default_value('[link_github]\n[commit]\n[link_commit]\n[username]')
+				        	->set_help_text('[username] untuk menampilkan username github. [commit] pesan commit git. [link_commit] url commit repository. [link_github] url repository github.')
+				    ) )
 	        ) );
 	}
 
+	function convert_links_for_parsing( $text ) {
+
+		$parse_mode = get_option( 'tg_mode' );
+
+		if ( 'Markdown' !== $parse_mode ) {
+			$text = preg_replace( '/\[([^\]]+?)\]\(([^\)]+?)\)/ui', '<a href="$2">$1</a>', $text );
+
+			if ( 'HTML' !== $parse_mode ) {
+				$text = wp_strip_all_tags( $text, false );
+			}
+		}
+		return $text;
+	}
 }
